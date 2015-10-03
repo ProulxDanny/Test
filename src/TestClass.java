db41a1bd313c4dc5ed2f6bfc8658329cabd7826a
dannyproulx <dannyproulx@192.168.0.102>
2015-10-02 21:54:46 -0400

    Je commit

--- src/TestClass.java
+++ src/TestClass.java
@@ -8,5 +8,35 @@
  * @author dannyproulx
  */
 public class TestClass {
-    
+
+    private int i;
+    private int j;
+
+    /**
+     * @return the i
+     */
+    public int getI() {
+        return i;
+    }
+
+    /**
+     * @param i the i to set
+     */
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    /**
+     * @return the j
+     */
+    public int getJ() {
+        return j;
+    }
+
+    /**
+     * @param j the j to set
+     */
+    public void setJ(int j) {
+        this.j = j;
+    }
 }
